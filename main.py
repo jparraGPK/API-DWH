ca3fd55b9685d248026b9282ce645c63d80cb527
jparraGPK <jparra@geo-park.com>
2025-04-21 21:06:40 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,45 +1,25 @@
 import azure.functions as func
 import logging
-import os
-import pyodbc
-import json
-import datetime
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
 @app.route(route="query")
-def query_sql(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info("Iniciando ejecución de consulta sobre SQL Pool")
+def query(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
 
-    query = req.params.get('query')
-    if not query:
+    name = req.params.get('name')
+    if not name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            name = req_body.get('name')
+
+    if name:
+        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    else:
         return func.HttpResponse(
-            "Falta el parámetro 'query'. Ejemplo: ?query=SELECT+TOP+10+*+FROM+yc.wellproddaily",
-            status_code=400
+             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             status_code=200
         )
-
-    try:
-        conn_str = os.environ.get("DB_CONNECTION_STRING")
-        if not conn_str:
-            return func.HttpResponse("No se encontró la variable DB_CONNECTION_STRING.", status_code=500)
-
-        with pyodbc.connect(conn_str) as conn:
-            cursor = conn.cursor()
-            cursor.execute(query)
-            columns = [col[0] for col in cursor.description]
-            rows = cursor.fetchall()
-            results = [dict(zip(columns, row)) for row in rows]
-
-        def convert(obj):
-            if isinstance(obj, (datetime.datetime, datetime.date)):
-                return obj.isoformat()
-            return str(obj)
-
-        return func.HttpResponse(
-            json.dumps(results, default=convert),
-            mimetype="application/json"
-        )
-
-    except Exception as e:
-        logging.error(f"Error ejecutando la consulta: {str(e)}")
-        return func.HttpResponse(f"Error: {str(e)}", status_code=500)
